436da59b7fddeaaf65bbf84fec21c1bc93fb37fd
Nedyalkov420 <dancheff@abv.bg>
2025-03-16 14:21:01 +0200

    Upload project

---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+
+
+function Header (){
+
+    return(
+        <header>
+        <section class="titles">
+            <h1 class="title">LB Commerce</h1>
+            <h3 class="sub-title">Car Parts Shop</h3>
+        </section>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.html">Начало</a>
+                </li>
+                <li>
+                    <a href="aboutus.html">За Нас</a>
+                </li>
+                <li>
+                    <a href="index.html">Продукти </a>
+                </li>
+                <li>
+                    <a href="contacts.html">Контакти</a>
+                </li>
+                <p class="menu">Menu</p>
+            </ul>
+        </nav>
+        </header>
+    );
+}
+
+export default Header
